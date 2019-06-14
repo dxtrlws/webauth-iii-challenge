01288e47bd8d9f1d7f0869084620bee783fdb037
Dexter Lewis <dexter.lewis@gmail.com>
2019-06-14 19:39:34 -0400

    mvp complete

--- client/src/App.js
+++ client/src/App.js
@@ -1,36 +1,42 @@
 import React from 'react';
-import { NavLink, Route } from 'react-router-dom';
-import './App.css';
+import { NavLink, Route, withRouter } from 'react-router-dom';
 import Signup from './components/Signup';
 import Login from './components/Login';
 import Users from './components/Users';
 
-function App() {
-  return (
-    <div className='App'>
-      <ul>
-        <li>
-          <NavLink to='/signup'>Sign Up</NavLink>
-        </li>
-        <li>
-          <NavLink to='/login'>Login</NavLink>
-        </li>
-        <li>
-          <NavLink to='/users'>Users</NavLink>
-        </li>
-        <li />
-        <li>
-          <NavLink to='/login'>Logout</NavLink>
-        </li>
-      </ul>
+import './App.css'
 
-      <main>
-        <Route path='/signup' component={Signup} />
-        <Route path='/login' component={Login} />
-        <Route path='/users' component={Users} />
-      </main>
-    </div>
-  );
+class App extends React.Component {
+  logout = () => {
+    localStorage.removeItem('token')
+    this.props.history.push('/login')
+  }
+  render() {
+    return (
+      <div className='App'>
+        <ul>
+          <li>
+            <NavLink to='/signup'>Sign Up</NavLink>
+          </li>
+          <li>
+            <NavLink to='/login'>Login</NavLink>
+          </li>
+          <li>
+            <NavLink to='/users'>Users</NavLink>
+          </li>
+          <li>
+            <button onClick={this.logout}>Logout</button>
+          </li>
+        </ul>
+
+        <main>
+          <Route path='/signup' component={Signup} />
+          <Route path='/login' component={Login} />
+          <Route path='/users' component={Users} />
+        </main>
+      </div>
+    );
+  }
 }
 
-export default App;
+export default withRouter(App);
